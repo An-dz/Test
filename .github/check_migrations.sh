3f7b1e4f2e399b0f5c0fd399a8a618c6ce1de5ee
André Zanghelini <An-dz@users.noreply.github.com>
2025-01-16 17:13:32 -0300

    Update check_migrations.sh

--- .github/check_migrations.sh
+++ .github/check_migrations.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-# Define the two lists (arrays)
-all_migrations=($(ls prisma/migrations))
-new_migrations=($(/c/Program\ Files/Sublime\ Merge/Git/cmd/git.exe status --porcelain | grep -Po "(?<=migrations/)\d+_[a-z_0-9]+"))
+# get the new migration files (in stage)
+new_migrations=($(git status --porcelain | grep -Po "(?<=migrations/)\d+_[a-z_0-9]+"))
+# get all migrations (existing + new)
+all_migrations=($(ls "$(git status --porcelain | grep -Po '^.*/migrations/(?=\d+_[a-z_0-9]+)' | awk 'NR == 1 {print $2}')"))
 
-# Iterate over list1 and remove items that are in list2
-current_migrations=()
+# iterate over all_migrations and remove items that are in new_migrations
+existing_migrations=()
 
 for item in "${all_migrations[@]}"; do
-    # Check if the item is in list2
+    # check if the item is in new_migrations
     if [[ ! " ${new_migrations[@]} " =~ " ${item} " ]]; then
-        current_migrations+=("$item")  # Add item to the current_migrations list if not in list2
+        existing_migrations+=("$item")  # Add item to existing_migrations list if not in new_migrations
     fi
 done
 
-if [[ ${new_migrations[0]:0:14} -lt ${current_migrations[-1]:0:14} ]]; then
+echo new: ${new_migrations[0]:0:14}
+echo cur: ${existing_migrations[-1]:0:14}
+
+# check if the oldest of the new migrations is before the latest of the existing migrations
+if [[ ${new_migrations[0]:0:14} -lt ${existing_migrations[-1]:0:14} ]]; then
+	echo There are new migrations with timestamp lower than the last migration in the destination branch
 	exit 1
 fi
