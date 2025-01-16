f8dfaf11302282b0093890a1699106936fcbaeab
André Zanghelini <An-dz@users.noreply.github.com>
2025-01-16 17:58:27 -0300

    test

--- .github/check_migrations.sh
+++ .github/check_migrations.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-git diff --name-only HEAD^1 HEAD
-
 # get the new migration files (in stage)
 new_migrations=($(git diff --name-only HEAD^1 HEAD | grep -Po "(?<=migrations/)\d+_[a-z_0-9]+"))
 # get all migrations (existing + new)
-all_migrations=($(ls "$(git diff --name-only HEAD^1 HEAD | grep -Po '^.*/migrations/(?=\d+_[a-z_0-9]+)' | awk 'NR == 1 {print $2}')"))
+migrations_paths = "$(git diff --name-only HEAD^1 HEAD | grep -Po '^.*/migrations/(?=\d+_[a-z_0-9]+)')"
+all_migrations=($(ls "$migrations_paths[0]"))
 
 # iterate over all_migrations and remove items that are in new_migrations
 existing_migrations=()
@@ -17,8 +16,8 @@ for item in "${all_migrations[@]}"; do
     fi
 done
 
-echo new: ${new_migrations[0]:0:14}
-echo cur: ${existing_migrations[-1]:0:14}
+echo applied : ${existing_migrations[-1]:0:14}
+echo previous: ${new_migrations[0]:0:14}
 
 # check if the oldest of the new migrations is before the latest of the existing migrations
 if [[ ${new_migrations[0]:0:14} -lt ${existing_migrations[-1]:0:14} ]]; then
